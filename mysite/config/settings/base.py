ebe52a64d656187f3386528ccfa149b1b7f8becb
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2018-11-17 22:14:42 +0900

    test production.py

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -108,11 +108,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = '/var/www/django/mysite/static/'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-     [os.path.join(PROJECT_ROOT, 'static')],
-)
