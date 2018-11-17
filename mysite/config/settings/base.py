6cd03941e91a8605a69fb02f5e0ed22c65ee0842
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2018-11-17 22:19:17 +0900

    test production.py

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -114,5 +114,5 @@ STATIC_ROOT = '/var/www/django/mysite/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-     [os.path.join(BASE_DIR, 'static')],
+     [os.path.join(BASE_DIR, 'static')]
 )
